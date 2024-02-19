89b0bc190c83ae9bfadbf849cbfb2f47a4ad3557
joleluis12 <jv1607547@gmail.com>
2024-02-19 09:27:22 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LayoutHome from './pages/home/layout/LayoutHome';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LayoutHome/>
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
